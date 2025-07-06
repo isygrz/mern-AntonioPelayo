38b000b2af9d91a2817c6a4b177930d5a4d00ce1
Israel Garza <isygarz@gmail.com>
2025-07-06 14:16:13 -0700

    C01-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Jalisco Tile</a>
+      </header>
+      <main>
+        <p>Featured Products</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
